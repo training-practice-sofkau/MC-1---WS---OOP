a1d9a1340f98ff3a212664cc1f059e611e830737
isan-san <isan.9.9f@gmail.com>
2023-01-23 17:14:42 -0500

    Develop vehicle and its sub clases

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -1,5 +1,8 @@
 package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Bicycle {
+public class Bicycle extends Vehicle {
+    public Bicycle(boolean available, String color, String state) {
+        super(available, color, state);
+    }
 }
--- src/co/com/movingu/vehicle/Vehicle.java
+++ src/co/com/movingu/vehicle/Vehicle.java
@@ -3,9 +3,13 @@ package co.com.movingu.vehicle;
 //TO DO: Complete/Rewrite the class according to the requirements
 public abstract class Vehicle implements Loanable{
     private boolean available;
+    private String color;
+    private String state;
 
-    public Vehicle(boolean available) {
+    public Vehicle(boolean available, String color, String state) {
         this.available = available;
+        this.color = color;
+        this.state = state;
     }
 
     @Override
