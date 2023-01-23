8bacb347587d7c7504ad63e01b6671dbf88dcb10
Julian Rojas <julianrojasgeo@gmail.com>
2023-01-23 14:05:53 -0500

    Ticket class sketch
    Main structure of the ticket class

--- src/co/com/movingu/ticket/Ticket.java
+++ src/co/com/movingu/ticket/Ticket.java
@@ -1,7 +1,31 @@
 package co.com.movingu.ticket;
 
+import co.com.movingu.user.User;
+import co.com.movingu.vehicle.Vehicle;
+
+import java.util.Date;
+
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Ticket {
+public class Ticket implements ITicket {
+
+    private String ticketID;
+    private Date ticketCreation;
+    private Date ticketExpiration;
+    private Boolean helmet;
+    private String status;
+    private Integer amountToPay;
+    private Vehicle vehicle;
+    private User user;
+
+
+    public Ticket() {
+    }
 
     //TO DO: Add the necessary getters & setters u other extra function
+    @Override
+    public Integer payTicket() {
+        return null;
+    }
+
+
 }
--- src/co/com/movingu/user/User.java
+++ src/co/com/movingu/user/User.java
@@ -2,15 +2,11 @@ package co.com.movingu.user;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public abstract class User {
-    //TO DO: Attributes
+
     protected String dni;
-
     protected String name;
-
     protected int age;
-
     protected boolean blocked;
-
     protected boolean ticketOn;
 
     public User(String dni, String name, int age) {
