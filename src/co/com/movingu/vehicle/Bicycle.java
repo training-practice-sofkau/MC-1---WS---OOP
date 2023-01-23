b97ff949e39752682486a587c827fd3bbc9fc7dd
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-23 15:54:30 -0500

    Complete the Bycicle Class

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -1,5 +1,16 @@
 package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Bicycle {
+public class Bicycle extends Vehicle{
+
+    private BicycleType type;
+    public Bicycle(String id, String color, boolean isAvailable, Condition condition, BicycleType type) {
+        super(id, color, isAvailable, condition);
+        this.type = type;
+    }
+
+    public enum BicycleType {
+        MOUNTAIN,
+        ROAD
+    }
 }
