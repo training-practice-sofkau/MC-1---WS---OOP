7a5a8d3980c69bb636153e4bc3a4884d29fd506b
Juangmcz <juangui-089@hotmail.com>
2023-01-23 14:58:19 -0500

    The class Scooter is done

--- src/co/com/movingu/vehicle/Scooter.java
+++ src/co/com/movingu/vehicle/Scooter.java
@@ -2,7 +2,22 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Scooter extends Vehicle{
-    public Scooter(boolean available) {
-        super(available);
+
+    private int batteryPercentage;
+
+    public Scooter(String code, String color, boolean isUsable, boolean isAvailable, int batteryPercentage) {
+        super(code, color, isUsable, isAvailable);
+        this.batteryPercentage = batteryPercentage;
+    }
+
+    @Override
+    public String toString() {
+        return "Scooter{" +
+                "batteryPercentage=" + batteryPercentage +
+                ", code='" + code + '\'' +
+                ", color='" + color + '\'' +
+                ", isUsable=" + isUsable +
+                ", isAvailable=" + isAvailable +
+                '}';
     }
 }
