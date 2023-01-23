42f0ca7d8cd79228897b798f1819fbda6830e24a
Diego Becerra <diegofer1110@gmail.com>
2023-01-23 15:32:27 -0500

    Modifying vehicle classes

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -1,5 +1,22 @@
 package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Bicycle {
+public class Bicycle extends Vehicle {
+
+    private String type;
+    public Bicycle(String codeVH,String color, boolean condition,String type) {
+        super(codeVH,color, condition);
+        this.type=type;
+    }
+
+    @Override
+    public void infoMsg() {
+
+        if (isAvailable()){
+            System.out.println("Bicycle "+getCodeVH()+"is available");
+        }else{
+            System.out.println("Bicycle "+getCodeVH()+" and type "+this.type+" is rented");
+        }
+
+    }
 }
--- src/co/com/movingu/vehicle/Vehicle.java
+++ src/co/com/movingu/vehicle/Vehicle.java
@@ -2,10 +2,19 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public abstract class Vehicle implements Loanable{
+
+    private String codeVH;
+
+    private String color;
+
+    private boolean condition;
     private boolean available;
 
-    public Vehicle(boolean available) {
-        this.available = available;
+    public Vehicle(String codeVH, String color, boolean condition) {
+        this.codeVH=codeVH;
+        this.color=color;
+        this.condition=condition;
+        this.available = true;
     }
 
     @Override
@@ -14,4 +23,37 @@ public abstract class Vehicle implements Loanable{
     }
     //TO DO: Attributes
 
+    public abstract void infoMsg();
+
+    public String getCodeVH() {
+        return codeVH;
+    }
+
+    public void setCodeVH(String codeVH) {
+        this.codeVH = codeVH;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isCondition() {
+        return condition;
+    }
+
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
