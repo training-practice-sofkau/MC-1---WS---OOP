a4fd509303850069d24d7be149fba93358811ef0
Diego Becerra <diegofer1110@gmail.com>
2023-01-23 15:04:22 -0500

    Making structure for ticket class

--- src/co/com/movingu/ticket/Ticket.java
+++ src/co/com/movingu/ticket/Ticket.java
@@ -1,7 +1,36 @@
 package co.com.movingu.ticket;
 
+import co.com.movingu.user.User;
+import co.com.movingu.vehicle.Vehicle;
+
+import java.time.LocalDateTime;
+
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Ticket {
+
+    private int idSecuence=1;
+
+    private String code;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private boolean helmetSupplied;
+
+    private String ticketStatus;
+
+    private double amountToPay;
+
+    private User uTicket;
+
+    private Vehicle vTicket;
+
+    public Ticket(boolean helmetSupplied, User uTicket, Vehicle vTicket) {
+        this.helmetSupplied = helmetSupplied;
+        this.uTicket = uTicket;
+        this.vTicket = vTicket;
+    }
 
     //TO DO: Add the necessary getters & setters u other extra function
 }
