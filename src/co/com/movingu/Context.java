74195234098884dded37cc6f6bfd10fb5bfa9756
isan-san <isan.9.9f@gmail.com>
2023-01-23 15:20:47 -0500

    Create context class to manage general information

---
+++ src/co/com/movingu/Context.java
@@ -0,0 +1,7 @@
+package co.com.movingu;
+
+public class Context {
+    public final String[] VEHICLES = new String[]{"bicycle", "scooter"};
+    public final String[] PENALTIES = new String[]{"Delayed return", "Lack of accessories", "Damaged helmet"};
+    public final int[] FEES = new int[]{3, 10, 5};
+}
