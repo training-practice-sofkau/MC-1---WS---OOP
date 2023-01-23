7729b6a928607e052d84d376e4a00c3d29ee4303
Juangmcz <juangui-089@hotmail.com>
2023-01-23 14:57:27 -0500

    The class Bicycle is done

--- src/co/com/movingu/vehicle/Bicycle.java
+++ src/co/com/movingu/vehicle/Bicycle.java
@@ -1,5 +1,12 @@
 package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
-public class Bicycle {
+public class Bicycle extends Vehicle{
+
+    private String type;
+
+    public Bicycle(String code, String color, boolean isUsable, boolean isAvailable, String type) {
+        super(code, color, isUsable, isAvailable);
+        this.type = type;
+    }
 }
