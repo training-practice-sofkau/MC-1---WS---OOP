c1b1a23a802e535b1d48fe20772020edb11bdf3e
BrianSammit <briansammit.cruz@gmail.com>
2023-01-23 18:00:59 -0500

    Add Vehicle constructor

--- src/co/com/movingu/vehicle/Vehicle.java
+++ src/co/com/movingu/vehicle/Vehicle.java
@@ -2,10 +2,16 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public abstract class Vehicle implements Loanable{
+    protected String code;
+    protected String color;
     private boolean available;
+    private boolean condition;
 
-    public Vehicle(boolean available) {
+    public Vehicle(String code, String color, boolean available, boolean condition) {
+        this.code = code;
+        this.color = color;
         this.available = available;
+        this.condition = condition;
     }
 
     @Override
