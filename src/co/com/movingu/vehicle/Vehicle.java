46b60881461419dd70c293bba5001e210a467491
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-23 15:53:16 -0500

    Finish the Vehicle class

--- src/co/com/movingu/vehicle/Vehicle.java
+++ src/co/com/movingu/vehicle/Vehicle.java
@@ -2,16 +2,27 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public abstract class Vehicle implements Loanable{
-    private boolean available;
 
-    public Vehicle(boolean available) {
-        this.available = available;
+    protected String id;
+    protected String color;
+    protected boolean isAvailable;
+    protected Condition condition;
+
+    public Vehicle(String id, String color, boolean isAvailable, Condition condition) {
+        this.id = id;
+        this.color = color;
+        this.isAvailable = isAvailable;
+        this.condition = condition;
     }
 
     @Override
     public void updateAvailability(boolean value) {
-        this.available = value;
+        this.isAvailable = value;
     }
-    //TO DO: Attributes
 
+    public enum Condition {
+        NEW,
+        NEAR_MINT,
+        GOOD
+    }
 }
