d476b5d6a4968af094f3a2c596048b69ce11091e
Juangmcz <juangui-089@hotmail.com>
2023-01-23 14:52:04 -0500

    The class Trainer is done

--- src/co/com/movingu/user/Trainer.java
+++ src/co/com/movingu/user/Trainer.java
@@ -2,7 +2,7 @@ package co.com.movingu.user;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Trainer extends User{
-    private String category;
+    private final String  category;
 
     public Trainer(String dni, String name, int age, String category) {
         super(dni, name, age);
@@ -12,10 +12,21 @@ public class Trainer extends User{
     @Override
     public void statusMsg() {
         if(blocked && !ticketOn){
-            System.out.println("The trainer "+name+"is blocked bc of debt");
+            System.out.println("The trainer " + name + "is blocked due to a debt");
         }else{
-            System.out.println("The trainer "+name+"is blocked bc he/she already borrow a vehicle");
+            System.out.println("The trainer " + name + "is blocked because he/she has already borrowed a vehicle.");
         }
+    }
 
+    @Override
+    public String toString() {
+        return "Trainer{" +
+                "category='" + category + '\'' +
+                ", dni='" + dni + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", blocked=" + blocked +
+                ", ticketOn=" + ticketOn +
+                '}';
     }
 }
