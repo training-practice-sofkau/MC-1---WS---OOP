80210fb50b7059a12690ccfda9165ce62f2244bc
Juangmcz <juangui-089@hotmail.com>
2023-01-23 17:07:29 -0500

    Attributes and methods were added.

--- src/co/com/movingu/ticket/Ticket.java
+++ src/co/com/movingu/ticket/Ticket.java
@@ -1,7 +1,49 @@
 package co.com.movingu.ticket;
 
-//TO DO: Complete/Rewrite the class according to the requirements
+import co.com.movingu.user.User;
+import co.com.movingu.vehicle.Bicycle;
+import co.com.movingu.vehicle.Scooter;
+import co.com.movingu.vehicle.Vehicle;
+
+import java.time.*;
+
+
 public class Ticket {
 
-    //TO DO: Add the necessary getters & setters u other extra function
+    static String code = "T_";
+    static int counter = 0;
+    private LocalDate pickDate;
+    private LocalDate returnDate;
+    private boolean isThereAHelmet = true;
+
+    private boolean isDamaged = false;
+
+    private boolean isNoTime = false;
+    private double overTime = 0;
+    private String status = "Active";
+    private int amountToPay = 0;
+    private User user;
+    private Vehicle vehicle;
+
+    public Ticket (){
+        counter++;
+        code += counter;
+        pickDate = LocalDate.now();
+
+    }
+
+    public int getDebt(){
+        if(!isThereAHelmet && vehicle.getClass() == Scooter.class){
+            amountToPay += 35;
+        }
+        if(!isThereAHelmet && vehicle.getClass() == Bicycle.class){
+            amountToPay += 25;
+        }
+        return amountToPay;
+    }
+
+    public void setReturnDate(){
+        this.returnDate = LocalDate.now();
+    }
+
 }
