b862659492e1afa54bbd35eb54d695fa568fdd38
andrscc <andrescc143@gmail.com>
2023-01-23 15:21:42 -0500

    The class Scooter was created. I added the attribute to know the battery level and create the corresponding constructor considering the attributes this class inherits from the Vehicle class

--- src/co/com/movingu/vehicle/Scooter.java
+++ src/co/com/movingu/vehicle/Scooter.java
@@ -2,7 +2,11 @@ package co.com.movingu.vehicle;
 
 //TO DO: Complete/Rewrite the class according to the requirements
 public class Scooter extends Vehicle{
-    public Scooter(boolean available) {
-        super(available);
+    private int batteryLevel;
+    public Scooter(String id, String color, boolean available, boolean condition, int batteryLevel) {
+
+        super(id, color, available, condition);
+        this.batteryLevel = batteryLevel;
+
     }
 }
